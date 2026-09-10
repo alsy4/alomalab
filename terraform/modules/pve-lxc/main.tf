636d675315ac4cc35eabcfc64c6fe69651c400a1
nixaloma <syahiralimi@gmail.com>
2026-09-10 18:40:27 +0800

    LXC Modules init

---
+++ terraform/modules/pve-lxc/main.tf
@@ -0,0 +1,46 @@
+resource "proxmox_virtual_environment_container" "this" {
+  node_name = var.node_name
+  vm_id     = var.vm_id
+
+  description  = var.description
+  unprivileged = var.unprivileged
+
+  cpu {
+    cores = var.cpu_core
+  }
+
+  memory {
+    dedicated = var.dedicated_memory
+    swap      = var.swap
+  }
+
+  disk {
+    datastore_id = var.datastore_id
+    size         = var.disk_size
+  }
+
+  operating_system {
+    type = "debian"
+    template_file_id = var.template_file_id
+  }
+
+  initialization {
+    hostname = var.hostname
+
+    ip_config {
+      ipv4 {
+        address = var.ipv4_address
+        gateway = var.gateway
+      }
+    }
+
+    user_account {
+      keys = var.ssh_public_keys
+    }
+  }
+
+  network_interface {
+    name = "eth0"
+    bridge = var.bridge
+  }
+}
---
+++ terraform/modules/pve-lxc/variables.tf
@@ -0,0 +1,95 @@
+variable "node_name" {
+  type        = string
+  description = "Name displayed on pve gui"
+}
+
+variable "hostname" {
+  type        = string
+  description = "Hostname as per node"
+}
+
+variable "vm_id" {
+  type        = number
+  description = "VM Id"
+}
+
+variable "description" {
+  type        = string
+  description = "What is the VM for?"
+  default     = "Managed by Terraform"
+}
+
+variable "template_file_id" {
+  type        = string
+  description = "Volume ID for LXC template"
+}
+
+variable "datastore_id" {
+  type = string
+  default = "local-lvm"
+}
+
+variable "disk_size" {
+  type        = number
+  description = "Size of the disk in Gi"
+  default     = 8 #Gi
+}
+
+variable "cpu_core" {
+  type    = number
+  default = 1
+}
+
+variable "dedicated_memory" {
+  type    = number
+  default = 1024
+}
+
+variable "floating_memory" {
+  type    = number
+  default = 1024
+}
+
+variable "swap" {
+  type = number
+  default = 0
+}
+
+variable "bridge" {
+  type = string
+  description = "Network interfaces"
+  default = "vmbr0"
+}
+
+variable "ipv4_address" {
+  type = string
+  default = "dhcp"
+}
+
+variable "gateway" {
+  type = bool
+  description = "IPv4 Gateway"
+  default = false
+}
+
+variable "ssh_public_keys" {
+  type = list(string)
+  default = []
+}
+
+variable "unprivileged" {
+  type = bool
+  default = true
+}
+
+variable "mount_points" {
+  type = list(
+    object({
+      volume = string
+      path = string
+      read_only = optional(bool, false)
+    })
+  )
+
+  default = []
+}
