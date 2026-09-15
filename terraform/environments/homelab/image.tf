e78e52e4ec54bf329bce710172bdd88747c0441a
nixaloma <syahiralimi@gmail.com>
2026-09-15 18:35:59 +0800

    Separate image into its own image
    So that pve doesn't have to install ubuntu-cloud-init multiple time for
    each node

---
+++ terraform/environments/homelab/image.tf
@@ -0,0 +1,8 @@
+resource "proxmox_download_file" "ubuntu_jammy" {
+  content_type = "import"
+  datastore_id = "local"
+  node_name    = var.proxmox_node
+
+  url       = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+  file_name = "jammy-server-cloudimg-amd64.qcow2"
+}
