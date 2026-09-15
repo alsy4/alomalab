9fa06122fc990b22db42cde4793b709b995e4cff
nixaloma <syahiralimi@gmail.com>
2026-09-15 18:34:49 +0800

    Edited sdn vars

--- terraform/modules/sdn/main.tf
+++ terraform/modules/sdn/main.tf
@@ -9,8 +9,9 @@ resource "proxmox_sdn_vnet" "k3s" {
 }
 
 resource "proxmox_sdn_subnet" "k3s" {
-  vnet = proxmox_sdn_vnet.k3s.id
-  cidr = "192.168.20.0/24"
+  vnet    = proxmox_sdn_vnet.k3s.id
+  cidr    = "192.168.20.0/24"
+  gateway = "192.168.20.1"
 }
 
 resource "proxmox_sdn_applier" "k3s" {
@@ -22,7 +23,7 @@ resource "proxmox_sdn_applier" "k3s" {
 }
 
 resource "proxmox_network_linux_bridge" "vmbr20" {
-  name = "vmbr20"
+  name      = "vmbr20"
   node_name = "proxmox"
-  address = "192.168.20.1/24"
+  address   = "192.168.20.1/24"
 }
---
+++ terraform/modules/sdn/outputs.tf
@@ -0,0 +1,3 @@
+output "vnet_id" {
+  value = proxmox_sdn_vnet.k3s.id
+}
