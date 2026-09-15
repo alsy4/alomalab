419dcdcebcbc8183862e99891d267e1bc3a2d226
nixaloma <syahiralimi@gmail.com>
2026-09-15 18:36:11 +0800

    Created k3s cp and worker01, worker02

--- terraform/environments/homelab/k3s.tf
+++ terraform/environments/homelab/k3s.tf
@@ -1,25 +1,63 @@
 module "k3s-cp-01" {
-  source = "../../modules/k3s"
-  depends_on = [ module.vmbr20 ]
+  source     = "../../modules/k3s"
 
-  name = "k3s-cp-01"
-  description = "k3s-control-plane"
-  tags = ["k3s"]
+  name         = "k3s-cp-01"
+  description  = "k3s-control-plane"
+  tags         = ["k3s"]
   proxmox_node = "proxmox"
-  vm_id = 20100
+  vm_id        = 20100
+  image_file_id = proxmox_download_file.ubuntu_jammy.id
 
   cores = 2
 
   dedicated_memory = 2048
-  floating_memory = 512
+  floating_memory  = 512
 
   size = 10
 
-  bridge = "vmbr20"
-  address = "192.168.20.100"
-  gateway = "192.168.20.1"
+  address = "192.168.0.200"
+  gateway = "192.168.0.1"
 
-  username = "root"
+  username        = "root"
   ssh_public_keys = var.ssh_public_key
-  root_password = var.root_password 
+  root_password   = var.root_password
+}
+
+locals {
+  k3s_workers = {
+    k3s-worker-01 = {
+      vm_id = 20101
+      address = "192.168.0.201"
+    }
+    k3s-worker-02 = {
+      vm_id = 20102
+      address = "192.168.0.202"
+    }
+  }
+}
+
+module "k3s-worker" {
+  for_each = local.k3s_workers
+  source     = "../../modules/k3s"
+
+  name         = each.key
+  description  = "k3s worker node"
+  tags         = ["k3s"]
+  proxmox_node = "proxmox"
+  vm_id        = each.value.vm_id
+  image_file_id = proxmox_download_file.ubuntu_jammy.id
+
+  cores = 1
+
+  dedicated_memory = 1024
+  floating_memory  = 512
+
+  size = 8
+
+  address = each.value.address
+  gateway = "192.168.0.1"
+
+  username        = "root"
+  ssh_public_keys = var.ssh_public_key
+  root_password   = var.root_password
 }
